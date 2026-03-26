94022d100fb57581c3c3db7caf4dfc6875b8a742
MGLabs <mailbox@kieran.cloud>
2026-03-26 18:51:31 -0500

    Update title scrolling condition for specific paths

--- frontend/ts/musiq/title-scrolling.ts
+++ frontend/ts/musiq/title-scrolling.ts
@@ -12,7 +12,7 @@ export function onReady() {
 }
 
 $(document).ready(() => {
-  if (!window.location.pathname.endsWith('musiq/')) {
+  if (!["/musiq/", "/p/"].includes(window.location.pathname)) {
     return;
   }
   onReady();
